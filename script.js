ee7a4a037c3a28eac210c0b29755c3ff8ae6c385
Sahnoune Aya <124534385+SahnouneAya@users.noreply.github.com>
2023-09-26 23:28:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.querySelector('#cart-btn').onclick = () => {
 }
 
 
+let bar = document.getElementById('bar');
+let nav = document.getElementById('nav');
+let close = document.getElementById('close')
+
+
+    bar.addEventListener('click', () => {
+      nav.classList.add('active');
+    })
+
+
+    close.addEventListener('click', () => {
+       nav.classList.remove('active');
+    })
+
+
